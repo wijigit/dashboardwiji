de1ee12a4ee8136424f557decf2723f7d8cf91ef
wijidev <wijiadrian@gmail.com>
2024-11-19 16:51:43 +0700

    lanjutan edit dashboard

--- app/Filament/Resources/MahasiswaResource.php
+++ app/Filament/Resources/MahasiswaResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\JurusanResource\Pages;
-use App\Filament\Resources\JurusanResource\RelationManagers;
-use App\Models\Jurusan;
+use App\Filament\Resources\MahasiswaResource\Pages;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+use App\Models\Mahasiswa;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,9 +13,9 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class JurusanResource extends Resource
+class MahasiswaResource extends Resource
 {
-    protected static ?string $model = Jurusan::class;
+    protected static ?string $model = Mahasiswa::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -23,7 +23,11 @@ class JurusanResource extends Resource
     {
         return $form
             ->schema([
+                Forms\Components\TextInput::make('nim'),
                 Forms\Components\TextInput::make('name'),
+                Forms\Components\Select::make('jurusan_id')
+                ->relationship('jurusan', 'name')
+                ->required(),
             ]);
     }
 
@@ -31,7 +35,9 @@ class JurusanResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('nim')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('jurusan.name')->searchable()->sortable(),
             ])
             ->filters([
                 //
@@ -56,9 +62,9 @@ class JurusanResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListJurusans::route('/'),
-            'create' => Pages\CreateJurusan::route('/create'),
-            'edit' => Pages\EditJurusan::route('/{record}/edit'),
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
         ];
     }
 }
--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Mahasiswa extends Model
 {
+
+    protected $fillable = [
+        'name',
+        'nim',
+        'jurusan_id'
+    ];
+
     public function jurusan(): BelongsTo
     {
         return $this->belongsTo(jurusan::class);
