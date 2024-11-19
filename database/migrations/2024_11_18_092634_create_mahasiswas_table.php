2439e3691770ea0fcd4bb791178928f08366c2e5
wijidev <wijiadrian@gmail.com>
2024-11-19 15:12:29 +0700

    ganti comment

--- database/migrations/2024_11_18_092634_create_mahasiswas_table.php
+++ database/migrations/2024_11_18_092634_create_mahasiswas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
             $table->string('nim');
+            //
             $table->string('name');
             $table->foreignId('jurusan_id')->constrained('jurusans')->cascadeOnDelete();
             $table->timestamps();
